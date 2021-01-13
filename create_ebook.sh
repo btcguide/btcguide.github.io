dcc2ba4bd96a3f2c5231f64b618ebb6a9fd619a1
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2021-01-13 17:25:10 +0100

    Rename index.md to index_pandoc.md and fix spellcheck

--- create_ebook.sh
+++ create_ebook.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pandoc --css templates/epub.css  --highlight-style zenburn --toc -N --lua-filter _pandoc_filter/image_link.lua  --lua-filter _pandoc_filter/add_title.lua  -o assets/btcguide.epub  index.md
+pandoc --css templates/epub.css  --highlight-style zenburn --toc -N --lua-filter _pandoc_filter/image_link.lua  --lua-filter _pandoc_filter/add_title.lua  -o assets/btcguide.epub  index_pandoc.md
